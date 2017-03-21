dbdf5b4885d3c63883425e32618731816469456c
James Fidler <terrarum@gmail.com>
2017-03-21 20:30:09 +0000

    Wrong server command.

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ if (process.env.NODE_ENV === "production") {
   require('dotenv').config();
 }
 
-var express = require('express');
-var app = express();
-var PORT = 4000;
-var staticOptions = {
+const express = require('express');
+const app = express();
+const PORT = 4000;
+const staticOptions = {
   maxAge: 2592000000 // 30 days
 };
+
 // Set up app.
 app.set('port', (process.env.PORT || PORT));
 app.use(express.static(__dirname + '/dist', staticOptions));
+
 // Set up routes.
 app.get('/', function(request, response) {
+
 });
+
 // Start web server and handle errors.
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
