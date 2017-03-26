874b95f7eb4871bbf7dbd668f0c93be08ae2514f
James Fidler <terrarum@gmail.com>
2017-03-26 02:40:32 +0100

    Overtime

--- src/components/GameDetails.vue
+++ src/components/GameDetails.vue
@@ -6,6 +6,7 @@
       <div class="games" v-for="game in activeSeries.games">
         <div class="game">
           {{ game.winner }} {{game.winnerScore}} - {{ game.loserScore }} {{ game.loser }}
+          <span v-if="game.isOvertime">(OT)</span>
         </div>
       </div>
       {{ activeSeries.note }}
